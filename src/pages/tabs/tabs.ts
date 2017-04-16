4e56328c10745325d102b40d7aef2bda03d795b6
peterennis <peterennis@yahoo.com>
2017-04-15 23:33:17 -0700

    change About page to Themes and remove lazy loaading decorators

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { ThemesPage } from '../themes/themes';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
@@ -10,7 +10,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = ThemesPage;
   tab3Root = ContactPage;
 
   constructor() {
